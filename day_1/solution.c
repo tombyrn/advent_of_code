026f33c76aca877dc695b4278bbadf7217a91046
tombyrn <thomasbyrnes02@gmail.com>
2025-12-01 23:44:14 -0500

    i am so goddamn stupid

--- day_1/solution.c
+++ day_1/solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
 
@@ -20,17 +21,29 @@ int main(int argc, char** argv) {
     char* line = calloc(256, sizeof(char));
     size_t size;
     
-    while(getline(&line, &size, file) > 0) {
+  while(getline(&line, &size, file) > 0) {
         direction = line[0];
         rotation = atoi(line+1);
+        int start_dial = dial;
 
         if(direction == 'L') {
-            dial -= rotation;
+            dial -= (rotation % 100);
             if(dial < 0)
-                dial = (100 + dial) % 100;
+                dial += 100;
         }
         if(direction == 'R') {
             dial = (dial + rotation) % 100;
+        }
+
+        num_zeros += rotation / 100;
+
+        int rem = rotation % 100;
+        if (rem > 0 && start_dial != 0) {
+            if(direction == 'L' && start_dial < rem)
+                    num_zeros++;
+            if(direction == 'R' && start_dial + rem > 100)
+                    num_zeros++;
+
         }
 
         if(dial == 0) num_zeros++;
